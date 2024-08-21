8c01bee67ee139bf38a5d87065a4e68fa9d9c992
pedro dias <pedro.dias@aluno.cefetmg.br>
2024-08-21 20:14:40 -0300

    feat[deleteOldFiles]: função para deletar imagens antigas e não sobrecarregar a aplicaçao

--- src/server.js
+++ src/server.js
@@ -2,11 +2,25 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const crypto = require("crypto");
+const fs = require("fs").promises;
 
-const uploadDirectory = path.join(__dirname, "api", "imgs-api");
+const uploadDirectory = path.join(__dirname, "api", "img");
+
+async function deleteOldFiles(directory) {
+    try {
+        const files = await fs.readdir(directory);
+        for (const file of files) {
+            const filePath = path.join(directory, file);
+            await fs.unlink(filePath);
+        }
+    } catch (err) {
+        console.error("Erro ao deletar arquivos antigos:", err);
+    }
+}
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+    destination: async function (req, file, cb) {
+        await deleteOldFiles(uploadDirectory);
         cb(null, uploadDirectory);
     },
     filename: function (req, file, cb) {
