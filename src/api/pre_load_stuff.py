cdef793a3b7e65b25d263fb47ef729616b18d1c0
jAzz-hub <joaogu.2001@gmail.com>
2024-08-21 21:34:50 -0300

    Adicionando função de classificação que gera json

--- src/api/pre_load_stuff.py
+++ src/api/pre_load_stuff.py
@@ -3,35 +3,78 @@ import tensorflow as tf
 import numpy as np
 import keras as K
 import sklearn as sk
+import shutil 
+import os
 from PIL import Image
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
 from keras import backend as K
+import json
+
+
 
 threshold=0.877976
 np.random.seed(42)
 tf.random.set_seed(42)
 
 # Leu o arquivo com os pesos iniciais do modelo:
-model = load_model('./models/M1_weights_epoch_90.h5')
+model = load_model('./json/M1_weights_epoch_90.h5')
 print('Loaded model from disk')
 
 #Compilou o modelo:
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 
-def classificadora(path, model):
-    resize_output = path[:-4] + '_128x128_' + path[-4:]
-    with Image.open(path) as picture:
-        resized_pic = picture.resize((128, 128), Image.LANCZOS)
-        resized_pic.save(resize_output)
+def classificadora(path, model, dir):
+    if dir == False:
+        resize_output = path[:-4] + '_128x128_' + path[-4:]
+        with Image.open(path) as picture:
+            resized_pic = picture.resize((128, 128), Image.LANCZOS)
+            resized_pic.save(resize_output)
 
-    sample = img_to_array(load_img(path=resize_output, color_mode="rgb", target_size=None))
-    processing_img = np.expand_dims(sample, axis=0)
-    processed_image = processing_img/255
+        sample = img_to_array(load_img(path=resize_output, color_mode="rgb", target_size=None))
+        processing_img = np.expand_dims(sample, axis=0)
+        processed_image = processing_img/255
 
-    prediction = model.predict(processed_image)
+        prediction = model.predict(processed_image)
 
-    return [resize_output, prediction[0][0], "wet" if (prediction[0] > threshold)[0] else "dry" ]
+        return [resize_output, prediction[0][0], "wet" if (prediction[0] > threshold)[0] else "dry" ]
+    
+    else:
+        classificados = {}
+        classificados['attributes'] = {}
+        counter = 0
+        for filename in os.listdir(path):
+            counter += 1
+            if filename.lower().endswith(('.jpg')):
+                img_path = os.path.join(path, filename)
 
-#print(classificadora('./imgs-api/test/wet/istockphoto-183808583-612x612.jpg',model))
+                with Image.open(img_path) as picture:
+                    resized_pic = picture.resize((128, 128), Image.LANCZOS)
+                    resized_pic.save(img_path)
+
+                sample = img_to_array(load_img(path=img_path, color_mode="rgb", target_size=None))
+                processing_img = np.expand_dims(sample, axis=0)
+                processed_image = processing_img/255
+                prediction = model.predict(processed_image)
+
+                file = {}
+                file['name'] = filename
+                file['classified_as'] = "wet" if (prediction[0] > threshold)[0] else "dry"
+                file['score'] = str(prediction[0][0])
+
+                classificados['attributes'][f'{counter}'] = file 
+
+    output_path = r'./json/classificados.json'
+    with open (output_path, 'w') as filepath:
+        json.dump(classificados, filepath, indent=4)
+
+classificadora('./imgs-api/test/wet/', model, True)
+
+#Função:
+    #Recebe:
+        #uma lista com todos os arquivos
+    
+    #Cria:
+        #um diretório com todos os arquivos
+        
